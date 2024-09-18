2e5f71d4b515972bf3348332cebc8f228f1d9e89
rawan.refaat <rawan.refaat@squadio.com>
2024-09-18 20:05:57 +0300

    add verifyHomePageIsLoaded function

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,6 +1,8 @@
 package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 
 
@@ -12,8 +14,16 @@ public class HomePage extends BaseClass{
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
+    public void verifyHomePageIsLoaded() {
+        String expectedTitle = "Home | interview";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle, "Home page title doesn't match expected!");
+    }
 
     public void goToShop() {
+        verifyHomePageIsLoaded();
+        WebElement shopElement = driver.findElement(shopLink);
+        Assert.assertTrue(shopElement.isDisplayed(), "Shop link is not displayed on the home page!");
         clickOnElement(driver, shopLink);
     }
 
